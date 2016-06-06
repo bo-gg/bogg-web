02b799e0e7bc9810d7c528c8612e22a10a32cd8b
Ben Friedland <ben174@gmail.com>
2016-06-05 17:57:27 -0700

    Registration form

--- app/scripts/controllers/registration.js
+++ app/scripts/controllers/registration.js
@@ -8,10 +8,34 @@
  * Controller of the boggWebApp
  */
 angular.module('boggWebApp')
-  .controller('RegistrationCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('RegistrationCtrl', function ($scope) {
+    $scope.weekGoal = '';
+    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+    $scope.payload = {
+      username: '',
+      email: '',
+      password: '',
+      daily_weight_goal: '',
+      height: '',
+      weight: '',
+      activity_factor: '1.2',
+      bogger: {
+        gender: 'M',
+        birthdate: '',
+        auto_update_goal: true
+      }
+    };
+    // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+
+
+    $scope.register = function () {
+      console.log($scope.payload);
+    };
+    
+    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+    $scope.calcGoal = function () {
+      $scope.payload.daily_weight_goal = $scope.weekGoal / 7;
+    };
+    // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+
   });
